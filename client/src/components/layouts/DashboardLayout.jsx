f3cb95b4475c4735d57214d68432da22fcdcb8a1
Erik Niilivirta <erik.niilivirta@siili.com>
2018-06-18 18:57:07 +0300

    ...

--- client/src/components/layouts/DashboardLayout.jsx
+++ client/src/components/layouts/DashboardLayout.jsx
@@ -3,15 +3,13 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { GameList, GameDetails, Profile, Game1 } from '../'
 
 const DashboardLayout = () => (
-  <React.Fragment>
-    <Switch>
-      <Route path="/dashboard" exact component={GameList} />
-      <Route path="/dashboard/game/:id" exact component={GameDetails} />
-      <Route path="/dashboard/game/1/play" component={Game1} />
-      <Route path="/dashboard/profile" exact component={Profile} />
-      <Redirect to="/dashboard" />
-    </Switch>
-  </React.Fragment>
+  <Switch>
+    <Route path="/dashboard" exact component={GameList} />
+    <Route path="/dashboard/game/:id" exact component={GameDetails} />
+    <Route path="/dashboard/game/1/play" component={Game1} />
+    <Route path="/dashboard/profile" exact component={Profile} />
+    <Redirect to="/dashboard" />
+  </Switch>
 )
 
 export default DashboardLayout
